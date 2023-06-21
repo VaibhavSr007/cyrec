d7bb3ccb5094f4812aa970d158733e31d106f170
Sarvesh Choudhary <sarveshc111@.com>
2023-06-21 23:12:46 +0530

    WroteDescription

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,10 +6,30 @@ import img5 from '../assets/contacts.webp';
 
 
 export const images = [
-    {img: img1, hoverText: "Click", desc: "vbbvv"},
-    {img: img2, hoverText: "Click", desc: "vbbvv"},
-    {img: img3, hoverText: "Click", desc: "vbbvv"}, 
-    {img: img4, hoverText: "Click", desc: "vbbvv"}, 
-    {img: img5, hoverText: "Click", desc: "vbbvv"}, 
+    {
+        img: img1, 
+        hoverText: "Click",
+        head: "Subdomain" ,
+        desc: "In the Domain Name System hierarchy, a subdomain is a domain that is a part of another domain. For example, if a domain offered an online store as part of their website example.com, it might use the subdomain shop.example.com."},
+    {
+        img: img2, 
+        hoverText: "Click", 
+        head: "WhoIs",
+        desc: "WHOIS is a query and response protocol that is used for querying databases that store an Internet resource's registered users or assignees. These resources include domain names, IP address blocks and autonomous systems, but it is also used for a wider range of other information"},
+    {
+        img: img3, 
+        hoverText: "Click", 
+        head:"Ip Geolocation",
+        desc: "In computing, Internet geolocation is software capable of deducing the geographic position of a device connected to the Internet. For example, the device's IP address can be used to determine the country, city, or ZIP code, determining its geographical location."}, 
+    {
+        img: img4, 
+        hoverText: "Click", 
+        head:"Email Verification",
+        desc: "A verification message is a type of email sent to a user or a client after they have taken a certain action. This action could be creating an account, signing up for a newsletter, voting in a competition, or creating a support ticket. A verification email should inform a user that they need to confirm their action."}, 
+    {
+        img: img5, 
+        hoverText: "Click", 
+        head:"Web Contacts",
+        desc: "Website contacts enable users to connect with website owners or administrators for various purposes such as inquiries, support requests, feedback, business opportunities, or collaboration. The specific contact information provided by a website can vary depending on the nature of the website and the organization behind it."}, 
 
 ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,12 +14,16 @@ export default function Home() {
         {images.map((imgDetails, imgIndex) => (
           <ReactCardFlip key={imgIndex} isFlipped={imgIndex === flipped} flipDirection='horizontal'>
             <div className='relative hover:cursor-pointer'>
-              <img className='w-72 h-56 md:w-96 md:h-72 bg-contain bg-center rounded-3xl' src={imgDetails.img} />
+              <img className='w-72 h-56 md:w-96 md:h-72 bg-contain bg-center rounded-3xl pointer-events-none' src={imgDetails.img} />
               <div className='absolute inset-0 bg-gradient-to-b from-transparent to-white rounded-3xl opacity-0 hover:opacity-100'>
                 <p onClick={() => setFlipped(imgIndex)} className='flex justify-center items-end w-full h-full'>{imgDetails.hoverText}</p>
               </div>
             </div>
-            <div onClick={() => setFlipped(-1)} className='w-72 h-56 md:w-96 md:h-72 flex justify-center items-center bg-[#64CCC5] rounded-3xl hover:cursor-pointer'>{imgDetails.desc}</div>
+            <div onClick={() => setFlipped(-1)} className='relative p-3 w-72 h-56 md:w-96 md:h-72 flex flex-col gap-3 justify-center items-center bg-[64CCC5] rounded-3xl hover:cursor-pointer'>
+              <img className='absolute inset-0 w-72 h-56 md:w-96 md:h-72 bg-contain bg-center rounded-3xl pointer-events-none opacity-20 shadow-2xl shadow-black' src={imgDetails.img} />
+              <h1 className='font-bold text-3xl'>{imgDetails.head}</h1>
+              <p className='font-semibold'>{imgDetails.desc}</p>
+            </div>
           </ReactCardFlip>
         ))}
       </div>
